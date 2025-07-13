5a82d5ac0dde2d4f65769facb411016427d683bc
kelvin0263-kelvin <tanchunkeat0263@gmail.com>
2025-07-13 18:05:48 +0800

    First commit

---
+++ database/migrations/2025_07_13_092616_add_is_admin_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) { // add make some changes to the users table
+            $table->boolean('is_admin')->after('remember_token')->default(false); // add a new column called is_admin after the remember_token column and set the default value to false
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
